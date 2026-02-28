9eacf93d1a955afde91652bb14c2ce5c0ec5d8c9
Vladislav Shivtsov <shivtsovvlad@gmail.com>
2026-03-01 00:14:49 +0500

    Initial version of voa-74 site

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function () {
+    // Маска телефона (простая): +7 (___) ___-__-__
+    const phone = document.querySelector('[data-phone]');
+    if (phone) {
+        phone.addEventListener('input', () => {
+            let v = phone.value.replace(/\D/g, '');
+            if (v.startsWith('8')) v = '7' + v.slice(1);
+            if (!v.startsWith('7')) v = '7' + v;
+            v = v.slice(0, 11);
+
+            const p = v.padEnd(11, '_').split('');
+            phone.value = `+${p[0]} (${p[1]}${p[2]}${p[3]}) ${p[4]}${p[5]}${p[6]}-${p[7]}${p[8]}-${p[9]}${p[10]}`.replace(/_+/g, '');
+        });
+    }
+
+    // Фейковая отправка формы (потом заменишь на реальную интеграцию)
+    document.querySelectorAll('form[data-lead-form]').forEach(form => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            alert('Заявка отправлена (демо). Подключим отправку в мессенджер/почту позже.');
+            form.reset();
+        });
+    });
+})();
